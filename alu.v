4bc3805c2006e457ef7f4a2de33600978878cdd0
vickymmcd <vickymmcd@gmail.com>
2018-10-02 14:56:05 -0400

    starting general structure for alu.v

--- alu.v
+++ alu.v
@@ -8,5 +8,9 @@ input[31:0]   operandA,
 input[31:0]   operandB,
 input[2:0]    command
 );
-	// Your code here
+  // pseudo right now, need to use lut or something to get subtract val
+	// example of what one will look like (I think)
+  structuralBitSlice bitslice1(result[0], carryout1, command, operandA[0], operandB[0], subtract);
+  structuralBitSlice bitslice2(result[1], carryout2, command, operandA[1], operandB[1], carryout1);
+  ...
 endmodule
