0e980836bfd5afba8d05b1d645f3befd1a770097
vickymmcd <vickymmcd@gmail.com>
2018-10-01 21:12:13 -0400

    including addition and subtraction and alu file

--- bitslice.v
+++ bitslice.v
@@ -5,61 +5,31 @@
 `define NOT not #50
 `define XOR xor #50
 
-module behavioralFullAdder
+module structuralBitSlice
 (
     output sum,
     output carryout,
-    input a,
-    input b,
-    input carryin
-);
-    // Uses concatenation operator and built-in '+'
-    assign {carryout, sum}=a+b+carryin;
-endmodule
-
-module structuralFullAdder
-(
-    output sum,
-    output carryout,
+    input control0,
+    input control1,
+    input control2,
     input a,
     input b,
     input carryin
 
 );
 
- wire AxorB, AxorBxorC, AxorBC, AB;
+ wire AxorB, AxorBxorC, AxorBC, AB, notControl1, notControl2, subtract, newB;
 
- `XOR xorGate(AxorB, a, b);
+ 'NOT notGate(notControl1, control1);
+ 'NOT notGate2(notControl2, control2);
+ 'AND andGate(subtract, control0, notControl1, notControl2);
+ 'XOR xorGate(newB, subtract, b);
+ `XOR xorGate1(AxorB, a, b);
  `XOR xorGate2(sum, AxorB, carryin);
- `AND andGate(AB, a, b);
+ `AND andGate1(AB, a, b);
  `AND andGate2(AxorBC, carryin, AxorB);
  `OR orGate( carryout, AB, AxorBC);
 
-
-
-endmodule
-
-
-module FullAdder4bit
-(
-  output[3:0] sum,  // 2's complement sum of a and b
-  output carryout,  // Carry out of the summation of a and b
-  output overflow,  // True if the calculation resulted in an overflow
-  input[3:0] a,     // First operand in 2's complement format
-  input[3:0] b      // Second operand in 2's complement format
-);
-    // Your Code Here
-
-    //reg a, b, carryin;
-    wire carryout0, carryout1, carryout2;
-
-    structuralFullAdder adder0(sum[0], carryout0, a[0], b[0], 1'b0);
-    structuralFullAdder adder1(sum[1], carryout1, a[1], b[1], carryout0);
-    structuralFullAdder adder2(sum[2], carryout2, a[2], b[2], carryout1);
-    structuralFullAdder adder3(sum[3], carryout, a[3], b[3], carryout2);
-
-    // Overflow is
-    `XOR xorGate(overflow, carryout, carryout2);
 
 
 endmodule
