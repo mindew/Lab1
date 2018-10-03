ebd6f3a57e2360f66ec1f4e21a77e0a7a4f61ffe
vickymmcd <vickymmcd@gmail.com>
2018-10-03 07:44:19 -0400

    adding overflow and carryout

--- alu.v
+++ alu.v
@@ -1,5 +1,7 @@
 `include "bitslice.v"
 
+`define XOR xor #20
+`define AND1 and #10
 `define AND3 and #30
 `define NOT not #10
 
@@ -32,5 +34,8 @@ input[2:0]    command
       structuralBitSlice bitslice2(result[i+1], carryout1, command, operandA[i+1], operandB[i+1], carryout2);
     end
   endgenerate
+
+  `XOR xorGate(overflow, carryout1, carryout2);
+  `AND1 andGate2(carryout, carryout1);
 
 endmodule
